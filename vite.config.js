de801f5d0e75d9c77d57710c551fb6d28da6d424
Aditya <adityarajpf715@gmail.com>
2026-07-23 04:15:34 +0530

    fix: remove PWA/service-worker temporarily to force fresh content on phones

--- vite.config.js
+++ vite.config.js
@@ -1,50 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg'],
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
-        cleanupOutdatedCaches: true,
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-cache',
-              expiration: { maxEntries: 10, maxAgeSeconds: 86400 }
-            }
-          }
-        ]
-      },
-      manifest: {
-        name: 'Aditya',
-        short_name: 'Aditya',
-        description: 'Academic companion for college life',
-        theme_color: '#0f0f1a',
-        background_color: '#0f0f1a',
-        display: 'standalone',
-        orientation: 'portrait',
-        start_url: '/',
-        icons: [
-          {
-            src: 'icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+    react()
   ]
 })
